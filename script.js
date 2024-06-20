08e4b58317f897f49ff5b89d87c76e77fcfa3027
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-20 15:21:58 +0530

    navbar responsive

--- script.js
+++ script.js
@@ -57,3 +57,38 @@ function swiperAnimation() {
 
 page3Animation();
 swiperAnimation();
+
+let menu = document.querySelector("#menu");
+let fullScreen = document.querySelector("#full-screen");
+let nav = document.querySelector("nav");
+let navImg = document.querySelector("nav img");
+let navHamOpen = document.querySelector("nav #menu #open");
+let navHamClose = document.querySelector("nav #menu #close");
+let flag = false;
+
+menu.addEventListener("click", () => {
+  if (flag === false) {
+    fullScreen.style.top = 0;
+    navImg.style.opacity = 0;
+    nav.style.paddingTop = "4vw";
+    nav.style.paddingBottom = "4vw";
+    nav.style.borderBottom = "1px";
+    nav.style.borderBottomStyle = "solid";
+    nav.style.borderBottomColor = "#00000033";
+    navHamOpen.style.display = "none";
+    navHamClose.style.display = "block";
+
+    flag = true;
+  } else {
+    fullScreen.style.top = "-100%";
+    navImg.style.opacity = 1;
+    nav.style.paddingTop = "8vw";
+    nav.style.paddingBottom = "8vw";
+    nav.style.borderBottom = 0;
+    navHamOpen.style.display = "block";
+    navHamClose.style.display = "none";
+    flag = false;
+  }
+});
+
+// Animated Hamburger Icon Logic
